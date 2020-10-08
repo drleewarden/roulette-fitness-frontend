5d7ca858229ce489a12b441fc52906cc34ff2da6
darryn <drleewarden@gmail.com>
2020-10-08 22:48:34 +1100

    added ga

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,6 @@ import withData from "../lib/apollo";
 import AppProvider from "../components/Context/AppProvider";
 import App, { Container } from "next/app";
 import React from "react";
-// import Router from "next/router";
-
-// import * as gtag from "../lib/gtag";
-// // Notice how we track pageview when route is changed
-// Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
 
 class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
